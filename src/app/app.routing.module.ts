0e1d90917db87db3fcc0452f22a22221915bae54
yamilzavala <zavalayamil9@gmail.com>
2022-10-18 14:41:30 -0300

    splitting routes

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { AuthGuard } from "./components/auth/auth.guard";
-import { LoginComponent } from "./components/auth/login/login.component";
-import { SignupComponent } from "./components/auth/signup/signup.component";
 import { TrainingComponent } from "./components/training/training.component";
 import { WelcomeComponent } from "./components/welcome/welcome.component";
 
 const routes: Routes = [
     {path: '', component: WelcomeComponent},
-    {path: 'signup', component: SignupComponent},
-    {path: 'login', component: LoginComponent},
+
     {path: 'training', component: TrainingComponent, canActivate: [AuthGuard]},
 ];
 
--- src/app/components/auth/auth.module.ts
+++ src/app/components/auth/auth.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from "@angular/core";
-import { FlexLayoutModule } from "@angular/flex-layout";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { BrowserModule } from "@angular/platform-browser";
-import { MaterialModule } from "src/app/material.module";
+import { ReactiveFormsModule } from "@angular/forms";
+
 import { SharedModule } from "../shared/shared.module";
+import { AuthRoutingModule } from "./auth.routing.module";
 import { LoginComponent } from "./login/login.component";
 import { SignupComponent } from "./signup/signup.component";
 
@@ -14,7 +13,8 @@ import { SignupComponent } from "./signup/signup.component";
     ],
     imports: [        
         ReactiveFormsModule,
-        SharedModule       
+        SharedModule,
+        AuthRoutingModule       
     ], 
     exports: []
 })
